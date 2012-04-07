cc20cef8f44fd19200b698d9b64104ebda5fb5b6
Zachary Bush <zmbush@berkeley.edu>
2012-04-06 18:29:20 -0700

    Update to make define work

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import xml.dom.minidom
 import urllib
 import json
 
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.debug = True
+flask.use_debugger = True
 
 @app.route('/info/<word>')
 def define(word):
@@ -14,7 +16,18 @@ def define(word):
         "&type=define&q="
   contents = urllib.urlopen(url + word).read()
   dom = xml.dom.minidom.parseString(contents);
-  return json.dumps(dom)
+  definitions = dom.getElementsByTagName("def");
+  retval = "<b>" + word + "</b><br />"
+  display = False
+  i = 1
+  for definition in definitions:
+    display = True
+    retval += "%d) %s<br />" % (i, definition.childNodes[0].data)
+    i += 1
+  if display:
+    return retval
+  else:
+    return ""
  
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
