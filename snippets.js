f41fbe27ac7d9c7afd6ff69cc4fe6ceed17a3e76
Zachary Bush <zmbush@berkeley.edu>
2012-05-18 17:46:54 -0700

    Update snippets

--- snippets.js
+++ snippets.js
@@ -6,11 +6,45 @@ for(i = 0; i < links.length; i++){
 
 // Add event listners to highlight thing we hover over
 items = document.getElementsByTagName("*")
+insideOf = new Array()
+currentlyHighlighted = null
 for(i = 0; i < items.length; i++){ 
-  items[i].onmouseover = function(event){ 
+  items[i].onfocus = function(event){ 
+    console.log(this)
+    insideOf.push(this)
+    if(currentlyHighlighted != null)
+      currentlyHighlighted.style.border = ""
     this.style.border = "1px solid red"
+    currentlyHighlighted = this
   }
-  items[i].onmouseout = function(event){
-    this.style.border = ""
+  /*
+  items[i].onblur = function(event){
+    // if(currentlyHighlighted != null)
+      // currentlyHighlighted.style.border = ""
+    if(insideOf.length > 0){
+      currentlyHighlighted = insideOf[insideOf.length - 1]
+      if(currentlyHighlighted != null)
+        currentlyHighlighted.style.border = "1px solid red"
+    }
   }
+  */
 }
+
+document.onmouseup = function(e){ 
+  target = document.elementFromPoint(e.clientX, e.clientY)
+  current = target
+  target.style.border = "1px solid green"; 
+  while(current != document && current.nodeName != "A"){
+    console.log(current)
+    current = current.parentNode;
+  }
+  if(current.nodeName == "A")
+    if(current.href.search("redirect_uri") != -1)
+      document.location = "http://www.zmbush.com"
+    else
+      document.location = current.href
+}
+
+
+
+
